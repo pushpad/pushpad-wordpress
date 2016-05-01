afe8aabf3ebdb0da31f5c2235acd115ac3efa2b8
Marco Colli <collimarco91@gmail.com>
2016-05-01 16:33:15 +0200

    Many improvements

---
+++ admin/pushpad-admin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Function for admin page
+ */
+function pushpad_admin() {
+?>
+
+<div class="wrap">
+	<h1>How to use Pushpad plugin:</h1>
+	<ul>
+		<li><b>Widget:</b> Pushpad Button</li>
+		<li><b>Shortcode:</b> [pushpad-button] or [pushpad-button subscribe="Subscribe" unsubscribe="Unsubscribe"]</li>
+		<li><b>Metabox:</b> Send push notifications whenever you post something new.</li>
+	</ul>
+
+  <p>Read <a href="https://pushpad.xyz/docs" target="_blank">the docs</a> to learn how to use Pushpad.</p>
+
+  <p><a href="https://pushpad.xyz/projects/" target="_blank" class="button button-primary">&#x1f517; Pushpad Dashboard</a></p>
+  
+</div>
+
+<?php
+}
