f6e0507baf754b32273ef784df6334bcf1c36bbf
collimarco <collimarco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-05-21 19:20:42 +0000

    Update version
    git-svn-id: https://plugins.svn.wordpress.org/pushpad-web-push-notifications/trunk@1421620 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pushpad.php
+++ pushpad.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pushpad - Web Push Notifications
  * Plugin URI: https://pushpad.xyz/docs/wordpress
  * Description: Real push notifications for your website. Uses the W3C Push API for Chrome and Firefox and supports Safari.
- * Version: 1.0.0
+ * Version: 1.0.2
  * Author: Pushpad
  * Author URI: https://pushpad.xyz
  * Text Domain: pushpad
