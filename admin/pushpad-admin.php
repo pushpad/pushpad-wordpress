fc681805c255ad8e11ae2f4eb8928335d7c18ff1
collimarco <collimarco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-02-14 12:27:55 +0000

    Improve text in welcome screen
    git-svn-id: https://plugins.svn.wordpress.org/pushpad-web-push-notifications/trunk@1595704 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/pushpad-admin.php
+++ admin/pushpad-admin.php
@@ -6,17 +6,23 @@ function pushpad_admin() {
 ?>
 
 <div class="wrap">
-	<h1>How to use Pushpad plugin:</h1>
-	<ul>
-		<li><b>Widget:</b> A button to let your visitors subscribe to push notifications.</li>
-		<li><b>Shortcode:</b> [pushpad-button] or [pushpad-button subscribe="Subscribe" unsubscribe="Unsubscribe"]</li>
-		<li><b>Metabox:</b> Send push notifications whenever you post something new.</li>
-	</ul>
-
-  <p>Read <a href="https://pushpad.xyz/docs" target="_blank">the docs</a> to learn how to use Pushpad.</p>
-
-  <p><a href="https://pushpad.xyz/projects/" target="_blank" class="button button-primary">Go to dashboard</a></p>
+	<h1>Getting started with the Pushpad plugin</h1>
+  <p>If you don't have a Pushpad account yet, you must <a href="https://pushpad.xyz/users/sign_up" target="_blank">create an account</a>.</p>
+  <p>Then go to <i>Pushpad -> Settings</i> to configure this plugin.</p>
   
+  <h2>1. Collecting subscriptions</h2>
+  <p>In order to send push notifications, you first need to collect subscribers.</p>
+  <p>Use at least one of the following methods.</p>
+  <h3>Widget</h3>
+  <p>Go to <i>Appearance -> Widgets</i> and add the <i>Pushpad Button</i>.</p>
+  <h3>Shortcode</h3>
+  <p>Inside any page or post you can use <code>[pushpad-button]</code> or <code>[pushpad-button subscribe="Subscribe" unsubscribe="Unsubscribe"]</code>.</p>
+  <h3>Browser prompt</h3>
+  <p>If you use Pushpad Pro, you can ask your visitors to subscribe to push notifications on page load by enabling an option in <i>Pushpad -> Settings</i>.</p>
+
+  <h2>2. Sending push notifications</h2>
+  <p>When you create a new post, this plugin displays an option to send a notification to the subscribers.</p>
+  <p>You can also send notifications from the <a href="https://pushpad.xyz/projects/" target="_blank">Pushpad dashboard</a>.</p>
 </div>
 
 <?php
